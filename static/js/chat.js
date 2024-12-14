5dc702ffe69dcffe0e2fce85f53de6ae8b7ffcfd
miftah1299 <jannat27120@gmail.com>
2024-12-14 15:19:03 +0600

    Refactor chat.js to establish socket connection and enhance message handling; update index.html title and add favicon

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,14 +1,19 @@
-const socket = io();
+const socket = io.connect("http://127.0.0.1:5000");
+
+socket.on("connect", function () {
+    console.log("Connected to the server");
+});
+
+socket.on("message", function (msg) {
+    console.log("Message received: " + msg);
+    const messageDiv = document.createElement("div");
+    messageDiv.textContent = msg;
+    document.getElementById("messages").appendChild(messageDiv);
+});
 
 function sendMessage() {
     const messageInput = document.getElementById("message-input");
     const message = messageInput.value;
     socket.send(message);
-    messageInput.value = '';
+    messageInput.value = "";
 }
-
-socket.on('message', function(msg) {
-    const messageDiv = document.createElement("div");
-    messageDiv.textContent = msg;
-    document.getElementById("messages").appendChild(messageDiv);
-});
